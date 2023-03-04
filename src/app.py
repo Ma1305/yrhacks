2ac5bcdb25f69e7b0cbd9bb978ce6495cd74ba3c
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-03-03 20:04:56 -0500

    database and some login page information

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 import sys
-from flask import Flask, render_template, request, redirect
+from datetime import timedelta
+from flask import Flask, render_template, request, redirect, session, flash
 from cs50 import SQL
+import helpers
 
-ip = ""
+# set up the flask app
+IP = ""
 app = Flask(__name__)
+
+SESSION_LIFESPAN = timedelta(days=3)
 
 # configure database
 try:
@@ -16,8 +21,25 @@ except Exception as e:  # when testing
 
 @app.route("/")
 @app.route("/home")
-def home_page():
+def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    session.permanent = True
+    session.permanent_life_time = SESSION_LIFESPAN
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # Reached using POST
+
+    # Ensure all fields are filled
+    if not request.form.get("student-number") or not request.form.get("password"):
+        flash("Student number and password can not be blank")
+        return render_template("login.html")
 
 
 if __name__ == "__main__":
